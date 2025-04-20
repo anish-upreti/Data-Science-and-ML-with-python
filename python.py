8bf3f02bc2fc399ba9584ccca588fd6990742a8d
Anish <anishupreti2002@gmail.com>
2025-04-20 19:56:32 +0545

    first commit

---
+++ python.py
@@ -0,0 +1,10 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def greet(self):
+        print(f"Hi, I’m {self.name} and I’m {self.age} years old.")
+
+p1 = Person("Diana", 25)
+p1.greet()
